7e13418926418db89e5acb4235e04c4a0aad1418
sithjaisong <sith.jaisong@gmail.com>
2014-07-10 14:35:09 +0800

    edit and chnage the names of variables

--- 3.analysis.WGCNA.R
+++ 3.analysis.WGCNA.R
@@ -1,33 +1,79 @@
+###############################################################################
+#'title         : Load survey data on SKEP Phase I
+#'date          : July, 2014
+#'purpose       : load data from the shared Google Drive which is the exel 
+#'                format
+#'writed by     : Sith Jaisong (s.jaisong@irri.org)
+#'contact       : International Rice Research Institute
+#'input         : data from the 2.cleaning the data
+#'output        : data frame and RData 
+###############################################################################
+#Load data saved from the 2.cleaning the data
+lnames <- load("selected.data.RData")
+lnames
 #####-- Load Package --#####
 library(WGCNA)
-options(stringsAsFactors = FALSE)
-
+options(stringsAsFactors = FALSE) # data frame created after executing that line will not auto-convert to factors
 ######-- Step 1: Loading Data --#####
 
-load("selected.data.RData")
-
 data <- selected.data
-
 
 #####-- Step 1.2 Clustering and d identication of outlier --#####
 
 # Analyse the cluster analysis by using package flashClust what the differnt between clust and falshClust ???
-sampleTree <- flashClust(dist(data), method = "average")
+clustaltree <- flashClust(dist(data), method = "average")
 
+## Check the outline with cluter analysis  
 
-## Have the cluster file plot the cluster and decorate the plot with the main title and the  
-plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5, cex.axis = 1.5, cex.main = 2)
+plot(clustaltree, 
+     main = "Clustering to detect outliers",
+     sub="",
+     xlab="",
+     cex.lab = 1.5,
+     cex.axis = 1.5,
+     cex.main = 2
+     )
+
+### combine the cluster of production situation, insect injuires and disease 
+### with the prodcution (yield) 
 
 yield.data <- subset(selected.data, select = yield)
 
 yieldColors <- numbers2colors(yield.data, signed = F)
 
-plotDendroAndColors(sampleTree, yieldColors, groupLabels = names(yield.data), main = "Sample dendrogram and yield heatmap")
+plotDendroAndColors(sampleTree,
+                    yieldColors,
+                    groupLabels = names(yield.data),
+                    main = "Dendrogram of PS and IP with yield heatmap"
+                    )
 
 
+#####----- Network Construction and Analysis-----#####
+# This package is used for weighted network construction
+# Call the network topology analysis function
+###############################################################################
+# WGCNA provide the automatic network construction 1 step                    
+# Constructing a weighted gene network entails the choice of the soft thresholding 
+# power β to which co-expression or co occureance 
+# similarity is raised to calculate adjacency 
+# Soft Thresold is the value to cutoff the correlation coeffienct and  #
+# reduce spares correaltion 
+# function pickSoftThreshold that performs the analysis of network topology and aids the
+# user in choosing a proper soft-thresholding power. 
+###############################################################################
+sft <- pickSoftThreshold(t(data), dataIsExpr = TRUE,
+                         RsquaredCut = 0.85, 
+                         powerVector = c(seq(1, 10, by = 1), seq(12, 20, by = 2)), 
+                         removeFirst = FALSE, nBreaks = 10, blockSize = NULL, 
+                         corFnc = cor, 
+                         corOptions = list(use = 'p'), 
+                         networkType = "unsigned",
+                         moreNetworkConcepts = FALSE,
+                         verbose = 0, 
+                         indent = 0)
 
 
-net.inj.datsurvey <- blockwiseModules(data, 
+net.data <- blockwiseModules(data, 
                                       power = 6, 
                                       TOMType = "unsigned", 
                                       minModuleSize = 1, 
@@ -41,60 +87,52 @@ net.inj.datsurvey <- blockwiseModules(data,
 
 ##
 
-mergedColors <- labels2colors(net.inj.datsurvey$colors) # How many module the data have
+mergedColors <- labels2colors(net.data$colors) # How many module the data have
 
 # Plot the dendrogram and the module colors underneath
 
-plotDendroAndColors(net.inj.datsurvey$dendrograms[[1]], 
-                    mergedColors[net.inj.datsurvey$blockGenes[[1]]], 
+plotDendroAndColors(net.data$dendrograms[[1]], 
+                    mergedColors[net.data$blockGenes[[1]]], 
                     "Module colors", 
                     dendroLabels = FALSE, 
                     hang = 0.03, 
                     addGuide = TRUE, 
                     guideHang = 0.05)
-powers = c(c(1:10), seq(from = 12, to=20, by=2))
-
-# Call the network topology analysis function
-
-sft <- pickSoftThreshold(t(data), dataIsExpr = TRUE,
-                         RsquaredCut = 0.85, 
-                         powerVector = c(seq(1, 10, by = 1), seq(12, 20, by = 2)), 
-                         removeFirst = FALSE, nBreaks = 10, blockSize = NULL, 
-                         corFnc = cor, 
-                         corOptions = list(use = 'p'), 
-                         networkType = "unsigned",
-                         moreNetworkConcepts = FALSE,
-                         verbose = 0, 
-                         indent = 0)
 
 # Plot the results:
 
-sizeGrWindow(9, 5)
+#Scale-free topology fit index as a function of the soft-thresholding power
 
-par(mfrow = c(1,2))
+plot(sft$fitIndices[,1],
+     -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     xlab="Soft Threshold (power)",
+     ylab="Scale Free Topology Model Fit,signed R^2",
+     type="n",
+     main = paste("Scale independence"))
 
-cex1 = 0.9
-
-# Scale-free topology fit index as a function of the soft-thresholding power
-
-plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2], xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n", main = paste("Scale independence"))
-
-text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2], labels=powers,cex=cex1,col="red")
+text(sft$fitIndices[,1],
+     -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     labels=powers,cex=cex1,col="red")
 
 # this line corresponds to using an R^2 cut-off of h
-
-abline(h=0.50,col="red")
-
 # Mean connectivity as a function of the soft-thresholding power
 
-plot(sft$fitIndices[,1], sft$fitIndices[,5], xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n", main = paste("Mean connectivity"))
+plot(sft$fitIndices[,1], 
+     sft$fitIndices[,5],
+     xlab="Soft Threshold (power)",
+     ylab="Mean Connectivity",
+     type="n",
+     main = paste("Mean connectivity"))
 
-text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
-
+text(sft$fitIndices[,1],
+     sft$fitIndices[,5],
+     labels=powers,
+     cex=cex1,
+     col="red")
 
 ##### Stpe 2.2 : One-step network construction and module detection --#####
 
-net.inj.datsurvey <- blockwiseModules(data, 
+net.data <- blockwiseModules(data, 
                                       power = 1, 
                                       TOMType = "unsigned", 
                                       minModuleSize = 1, 
@@ -106,36 +144,33 @@ net.inj.datsurvey <- blockwiseModules(data,
                                       saveTOMFileBase = "inj.datsurveyTOM", 
                                       verbose = 3)
 
-##
+##How many module the data have
 
-mergedColors <- labels2colors(net.inj.datsurvey$colors) # How many module the data have
+mergedColors <- labels2colors(net.data$colors) 
 
 # Plot the dendrogram and the module colors underneath
 
-plotDendroAndColors(net.inj.datsurvey$dendrograms[[1]], 
-                    mergedColors[net.inj.datsurvey$blockGenes[[1]]], 
+plotDendroAndColors(net.data$dendrograms[[1]], 
+                    mergedColors[net.data$blockGenes[[1]]], 
                     "Module colors", 
                     dendroLabels = FALSE, 
                     hang = 0.03, 
                     addGuide = TRUE, 
                     guideHang = 0.05)
 
-#### Note: Module color = grey ####
-####
-
-moduleLabels <- net.inj.datsurvey$colors
-moduleColors <- labels2colors(net.inj.datsurvey$colors)
+moduleLabels <- net.data$colors # colors represent module
+moduleColors <- labels2colors(net.data$colors)
 
 # Define numbers of variables and samples
-nGenes <- ncol(data)
-nSamples <- nrow(data)
+nVars <- ncol(data)
+nFields <- nrow(data)
 # Recalculate MEs with color labels
 MEs0 <- moduleEigengenes(data, moduleColors)$eigengenes
 MEs <- orderMEs(MEs0)
 
 ###
 moduleVariablesCor <- cor(MEs, yield.data, use = "p")
-moduleVariablesPvalue <- corPvalueStudent(moduleVariablesCor, nSamples)
+moduleVariablesPvalue <- corPvalueStudent(moduleVariablesCor, nFields)
 
 ###
 sizeGrWindow(10,6)
@@ -170,7 +205,7 @@ modNames = substring(names(MEs), 3)
 
 VarModuleMembership <- as.data.frame(cor(data, MEs, use = "p"))
 
-MMPvalue <- as.data.frame(corPvalueStudent(as.matrix(VarModuleMembership), nSamples))
+MMPvalue <- as.data.frame(corPvalueStudent(as.matrix(VarModuleMembership), nFields))
 
 names(VarModuleMembership) = paste("MM", modNames, sep="")
 
@@ -178,7 +213,7 @@ names(MMPvalue) = paste("p.MM", modNames, sep="")
 
 VarYieldSignificance = as.data.frame(cor(data, yield.data, use = "p"))
 
-GSPvalue <- as.data.frame(corPvalueStudent(as.matrix(VarYieldSignificance), nSamples))
+GSPvalue <- as.data.frame(corPvalueStudent(as.matrix(VarYieldSignificance), nFields))
 
 names(VarYieldSignificance) = paste("GS.", names(yield), sep="")
 
@@ -186,11 +221,10 @@ names(GSPvalue) = paste("p.GS.", names(yield), sep="")
 
 #####-- Step 3 : Intramodular analysis: identifying variables within Module and yields
 
-module = "turquoise"
+module = "turquoise" # grey, yellow, blue, brown
 column = match(module, modNames)
+
 moduleGenes = moduleColors==module
-#sizeGrWindow(7, 7)
-#par(mfrow = c(1,1))
 
 verboseScatterplot(abs(VarModuleMembership[moduleGenes, column]),
                    abs(VarYieldSignificance[moduleGenes, 1]),
